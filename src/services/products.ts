f6c8c7df8a8f62f8bac51789dc5853f2e675c363
charles.labit <charleslabit@awesomelabph.com>
2025-03-15 20:27:57 +0800

    comment out sortby in fetchproduts ;test purposes

--- src/services/products.ts
+++ src/services/products.ts
@@ -4,8 +4,8 @@ import { Product, ProductsProps } from "@/types";
 export function fetchProducts({
   categoryId,
   search,
-  sortBy,
-}: ProductsProps): Promise<Product[]> {
+}: // sortBy,
+ProductsProps): Promise<Product[]> {
   return new Promise((resolve) => {
     setTimeout(() => {
       const filteredProducts = mockProducts?.filter((product) => {
@@ -18,16 +18,16 @@ export function fetchProducts({
         return matchesCategory && matchesSearch;
       });
 
-      // Sorting logic
-      if (sortBy) {
-        filteredProducts.sort((a, b) => {
-          if (sortBy === "name-asc") return a?.name?.localeCompare(b?.name);
-          if (sortBy === "name-desc") return b?.name.localeCompare(a?.name);
-          if (sortBy === "price-asc") return a?.price - b?.price;
-          if (sortBy === "price-desc") return b?.price - a?.price;
-          return 0;
-        });
-      }
+      // // Sorting logic
+      // if (sortBy) {
+      //   filteredProducts.sort((a, b) => {
+      //     if (sortBy === "name-asc") return a?.name?.localeCompare(b?.name);
+      //     if (sortBy === "name-desc") return b?.name.localeCompare(a?.name);
+      //     if (sortBy === "price-asc") return a?.price - b?.price;
+      //     if (sortBy === "price-desc") return b?.price - a?.price;
+      //     return 0;
+      //   });
+      // }
 
       resolve(filteredProducts);
     }, 0);
